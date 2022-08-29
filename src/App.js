c873af749d86e3e5398e147e1dce0372f01310f6
haymaker00 <nileshkumar4032@gmail.com>
2022-08-30 02:40:41 +0530

    1st

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import {Data} from './data';
+import {useState,useEffect, useSyncExternalStore} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+
+const App = ({label, key, type,options}) => {
+
+
+let value;
+switch(type) {
+  case 'input':
+    value={
+      input:"input"
+    }
+    break;
+    case 'dropdown':
+      value={
+        options:["a","b","c"]
+      }
+      break;
+      case 'radio':
+        value={
+       options:["e","d","c"]
+        }
+       break;
+default:
+  break
 }
 
-export default App;
+
+  return (
+    <div>
+       <div>
+       
+
+
+        return(
+        <form className='form' >
+        
+          <label>
+            
+            <input type="text" name="name" value= "input"/>{value.input}
+              <div className='radio'>
+            <input type="radio" value ="male" name="status" />{value.options[0]}
+            <input type="radio" value = "female"name="status" />{value.options[1]}
+            <input type="radio" value ="other" name="status"/>{value.options[2]}
+            </div>
+            <div className="dropdown">
+              <select name='select' id="select">
+                <option value="approved">{value.options[0]}</option>
+                <option value="pending" >{value.options[1]}</option>
+              </select>
+            </div>
+            <input type="submit" value="Submit"/>
+          </label>
+          
+        </form>
+        )
+    
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,34 @@
+ export const Data=[{
+    label:"Test 1",
+    key:"test1",
+    type:"input"
+},
+{
+    label:"Test 2",
+    key:"test2",
+    type:"dropdown",
+    options:["a","b","c"]
+},
+{
+    label:"Test 3",
+    key:"test3",
+    type:"dropdown",
+    options:["e","d","c"]
+},
+{
+    label:"Test 4",
+    key:"test4",
+    type:"radio",
+    options:["a","b","c"]
+},
+{
+    label:"Test 5",
+    key:"test5",
+    type:"input"
+},
+{
+    label:"Test 6",
+    key:"test6",
+    type:"radio",
+    options:["e","d","c"]
+}];
